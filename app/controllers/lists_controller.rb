bf7b5b852bb940f3417ba6bc6915cd3092c63363
YukiMatsuo <padosyu1127@gmail.com>
2024-06-20 12:50:23 +0000

    main

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,7 +2,15 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
-
+  
+  def create
+    list = List.new(list_params)
+    
+    list.save
+    
+    redirect_to '/top'
+  end
+  
   def index
   end
 
@@ -11,4 +19,11 @@ class ListsController < ApplicationController
 
   def edit
   end
+  
+    private
+  
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+
 end
